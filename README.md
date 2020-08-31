0fb2b38f882f16cdbf7d468bcbb35347dcf750d3
Nicholas Otieno <niklasoti@gmail.com>
2020-08-31 20:53:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Driven Development
+
+Learning Test Driven Development with mocha.
---
+++ index.test.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+const { forEach, map } = require("./index");
+
+it("The forEach function", () => {
+  let sum = 0;
+
+  forEach([1, 2, 3], (value) => {
+    sum += value;
+  });
+
+  assert.strictEqual(sum, 6, "Expected summing array to equal 6");
+});
+
+it("The map function", () => {
+  const result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
+  assert.strictEqual(result[0], 2);
+  assert.strictEqual(result[1], 4);
+  assert.strictEqual(result[2], 6);
+});
